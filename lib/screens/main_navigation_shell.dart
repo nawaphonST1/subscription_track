dba9e8220f83840ccebcb627ead26c5a6c6dd00d
Nekokun2004 <neitwut2004@gmail.com>
2026-08-06 23:18:15 +0700

    refactor: move settings into feature module

--- lib/screens/main_navigation_shell.dart
+++ lib/screens/main_navigation_shell.dart
@@ -8,7 +8,7 @@ import 'package:subscription_track/providers/main_navigation_provider.dart';
 import 'package:subscription_track/features/dashboard/presentation/dashboard_tab.dart';
 import 'package:subscription_track/features/profile/presentation/profile_tab.dart';
 import 'package:subscription_track/features/savings/presentation/savings_tab.dart';
-import 'package:subscription_track/screens/tabs/setting_tab.dart';
+import 'package:subscription_track/features/settings/presentation/settings_tab.dart';
 
 class MainNavigationShell extends ConsumerWidget {
   const MainNavigationShell({super.key});
